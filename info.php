babeabc80e1c7eed2d4271aa9be4c4e6dd8ea31d
amansitlab-cloud <amansitlab@gmail.com>
2026-06-29 13:31:14 +0530

    Second Update

--- info.php
+++ info.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Git Code Push</title>
+    <title>Document</title>
 </head>
 <body>
-    <h2> How to push code on github?</h2>
-    <h3> Add nev bar</h3>
+    
 </body>
 </html>
